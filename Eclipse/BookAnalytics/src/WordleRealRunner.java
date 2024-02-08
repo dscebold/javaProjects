49a9679fe2740ba6c5084de3227cbdd15a2bc106
Drew Scebold <dscebold638@gmail.com>
2024-02-08 12:55:52 -0600

    Why do these exist
    Why do these exist

--- Eclipse/BookAnalytics/src/WordleRealRunner.java
+++ Eclipse/BookAnalytics/src/WordleRealRunner.java
@@ -2,7 +2,7 @@
 public class WordleRealRunner {
 
 	public static void main(String[] args) {
-		WordleRunner wordle = new WordleRunner("Wordle.txt", "horny.txt"); 
+		WordleRunner wordle = new WordleRunner("Eclipse/BookAnalytics/Wordle.txt", "horny.txt");
 		wordle.playGame();
 
 	}
